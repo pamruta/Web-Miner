f5241904039a467000304c7ff20d4e1c88906484
Amruta Purandare <pamruta@Amrutas-MacBook-Air.local>
2017-03-31 10:33:20 +0530

    python script to fetch html page from a given url and convert it to plain-text

--- fetch-html.py
+++ fetch-html.py
@@ -1,9 +1,15 @@
+# Usage:
+# python fetch_html.py WEB_URL
+
 # importing libraries
 from bs4 import BeautifulSoup
 import urllib2
 
+import sys
+web_url = sys.argv[1]
+
 # fetch html page from url
-html_text = urllib2.urlopen("https://en.wikipedia.org/wiki/Jennifer_Aniston").read()
+html_text = urllib2.urlopen(web_url).read()
 
 soup = BeautifulSoup(html_text, "html.parser")
 text = ""
@@ -23,11 +29,9 @@ for para in soup.find_all("p"):
 import re
 text = re.sub(r'\[\d+\]', "", text)
 
-# handling of UTF-8 encoded characters
-import sys
+# handling UTF-8 encoded characters
 reload(sys)
-sys.setdefaultencoding("UTF8")
+sys.setdefaultencoding("UTF-8")
 
-# writing text to file
-output_file = open("Aniston.txt", "w")
-output_file.write(text)
+# printing text to std-out
+print text
