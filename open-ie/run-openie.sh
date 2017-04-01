edad74ee57d29de1a32003d93ea59c232dce3658
Amruta <pamruta@live.com>
2017-04-01 14:19:30 +0530

    phrase-extraction using Stanford-OpenIE tool

--- open-ie/run-openie.sh
+++ open-ie/run-openie.sh
@@ -2,6 +2,7 @@
 
 # Usage: ./run-openie.sh input-file
 
+# directory where stanford-corenlp package is installed
 set stanford_corenlp = "/Users/pamruta/Downloads/stanford-corenlp-full-2016-10-31"
 
 java -mx2g -cp "$stanford_corenlp/*" edu.stanford.nlp.naturalli.OpenIE -format "default" $1
